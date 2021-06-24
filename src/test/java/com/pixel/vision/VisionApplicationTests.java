6573c2254d29e65ca48fb4b0d07bf28129a79a3d
Abhit Kumar Das <abhit.das@meddo.in>
2021-06-24 09:10:09 +0530

    testcase

--- src/test/java/com/pixel/vision/VisionApplicationTests.java
+++ src/test/java/com/pixel/vision/VisionApplicationTests.java
@@ -1,13 +1,34 @@
 package com.pixel.vision;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class VisionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void getDayFromGivenDate() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/calendar/day")
+				.param("date", "06/24/2021")
+				.contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(content().json("{'day':'175','dayOfWeek':'THURSDAY'}"));
+	}
 }
